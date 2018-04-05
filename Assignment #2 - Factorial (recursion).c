144d51ba9100663557d4beece42c4ee53398b65b
jemtca <30645648+jemtca@users.noreply.github.com>
2018-04-04 23:10:53 -0700

    C Development

--- Assignment #2 - Factorial (recursion).c
+++ Assignment #2 - Factorial (recursion).c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+int clean_stdin(){
+    
+    while (getchar() != '\n');
+    
+    return 1;
+    
+}
+
 int factorial(int i){
     
     if(i <= 1){
@@ -16,14 +24,18 @@ int main(void) {
     
     int input;
     int result;
+    char c;
     
-    printf("Type a number to calculate the factorial: ");
-    scanf("%d",&input);
+    do{
+        
+        printf("Type a number to calculate the factorial: ");
+        
+    }while(((scanf("%d%c", &input, &c) != 2 || c != '\n') && clean_stdin()) || input < 1);
     
     result = factorial(input);
     
     if(result >= 1){
-    
+        
         printf("%d\n", result);
         
     }
--- Assignment #2 - Fibonacci (recursion).c
+++ Assignment #2 - Fibonacci (recursion).c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+int clean_stdin(){
+    
+    while (getchar() != '\n');
+    
+    return 1;
+    
+}
+
 int fibonacci(int i){
     
     if(i == 0){
@@ -21,18 +29,22 @@ int fibonacci(int i){
 int main(void) {
     
     int input;
+    char c;
     
-    printf("Type a number to calculate its fibonacci secuence: ");
-    scanf("%d", &input);
+    do{
+        
+        printf("Type a number to calculate its fibonacci secuence: ");
+        
+    }while(((scanf("%d%c", &input, &c) != 2 || c != '\n') && clean_stdin()) || input < 0);
     
-    for(int i = 0; i < input; i++){
+    for(int i = 0; i <= input; i++){
         
         printf("%d",fibonacci(i));
         
-        if(i < input - 1){
-        
+        if(i < input){
+            
             printf(", ");
-        
+            
         }
         else{
             
